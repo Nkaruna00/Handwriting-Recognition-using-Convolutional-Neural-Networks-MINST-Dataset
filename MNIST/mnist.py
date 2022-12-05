19b6571e3f8ccc173d99145ca3053916c74fba9d
Nkaruna00 <nithushankarunakaran@gmail.com>
2022-12-06 00:13:52 +0100

    Readme and comments

--- MNIST/mnist.py
+++ MNIST/mnist.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+
+
+# Import the libraries
 import keras
 from keras.datasets import mnist
 from keras.utils import np_utils
@@ -13,17 +16,24 @@ from keras.models import load_model
 from keras.preprocessing.image import img_to_array
 from keras import regularizers
 
+
+# Load the CIFAR10 Dataset
+
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
 y_train = np_utils.to_categorical(y_train,10)
 y_test =  np_utils.to_categorical(y_test,10)
 
+
+# Set the L2 Regularizers weights
 poids= 0.0005
 
 
 x_train = x_train.reshape((x_train.shape[0], 28, 28, 1))
 x_test = x_test.reshape((x_test.shape[0], 28, 28, 1))
 
+
+# Augment the data to have more images for training
 data_augmentee = ImageDataGenerator(
     rotation_range=15,
     width_shift_range=0.1,
@@ -33,10 +43,13 @@ data_augmentee = ImageDataGenerator(
 data_augmentee.fit(x_train)
 
 
+# Build the model
+
 model = Sequential()
 model.add(Convolution2D(32, (3,3), padding='same',kernel_regularizer=regularizers.l2(poids), input_shape=(28,28,1)))
 model.add(Activation('relu'))
 model.add(BatchNormalization())
+
 model.add(Convolution2D(32, (3,3), padding='same'))
 model.add(Activation('relu'))
 model.add(BatchNormalization())
@@ -46,6 +59,7 @@ model.add(Dropout(0.2))
 model.add(Convolution2D(64, (3,3), padding='same', kernel_regularizer=regularizers.l2(poids)))
 model.add(Activation('relu'))
 model.add(BatchNormalization())
+
 model.add(Convolution2D(64, (3,3), padding='same', kernel_regularizer=regularizers.l2(poids)))
 model.add(Activation('relu'))
 model.add(BatchNormalization())
@@ -54,17 +68,20 @@ model.add(Dropout(0.3))
 
 
 model.add(Flatten())
-#model.add(Dense(100, activation='relu'))
 model.add(Dense(10, activation='softmax'))
 
-
+# Compile the model with Adam Optimizers
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
+# TRain the model
 model.fit_generator(data_augmentee.flow(x_train, y_train, 1),steps_per_epoch = x_train.shape[0],epochs=3,verbose=1,validation_data=(x_test,y_test))
 
 #model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=epochs, batch_size=3)
-model.save('model_regu_l2.h5')
 
+# Save the model in a h5file
+model.save('mnist.h5')
+
+# Evaluate the accuracy of the model
 scores = model.evaluate(x_test, y_test, verbose=0)
 
 print("Accuracy: %.2f%%" % (scores[1]*100))
--- README.md
+++ README.md
@@ -1,1 +1,60 @@
 # ConvNet-MINST
+Creation of a convolutional neural network in order to recognize handwriting in an image and give its meaning.
+Trained with the MINST dataset.
+The code is written in Python and use the Keras and Tensorflow frameworks.
+
+
+## Description
+
+The dataset consists of 70,000 images (60,000 training and 10,000 test).
+The images contain images of numbers from 0 to 9 written in handwriting.  
+
+The data is augmented with the help of data augmentation tools to get more training data using rotations and flips among others.  
+
+The model consists of 5 layers.
+
+* The first layer consists of a convolution layer with Weight Decay, also called L2 Regularization. The activation function used is the rectifier (ReLU).
+Batch normalization is applied to improve the stability  
+
+* The second layer consists of a convolution layer with L2 Regularization. The activation function used is the rectifier (ReLU).
+Batch normalization is applied to improve the stability.
+MaxPooling is applied to downsample the input along its spatial dimensions.
+We also apply a Dropout of 0.2 which randomly sets input units to 0 with a frequency of rate at each step during training time, which helps prevent overfitting.  
+
+* The third layer consists of a convolution layer with L2 Regularization. The activation function used is (ReLU) with Batch Normalization.  
+
+* The fourth layer consists of a convolution layer with L2 Regularization. The activation function used is (ReLU).
+We apply Batch Normalization to improve the stability.
+MaxPooling is applied to downsample the input along its spatial dimensions.
+We also apply a Dropout of 0.3 which randomly sets input units to 0 with a frequency of rate at each step during training time, which helps prevent overfitting.  
+
+* The last layer is composed of 10 neurons (one for each object class.
+The activation function used is softmax.  
+
+* The optimizer used is Adam.
+
+* The mnist.h5 contains the trained model with all the weights.
+
+## Getting Started
+
+### Dependencies
+
+* Python
+* Keras with Tensorflow Backend
+
+### Executing program
+
+* Run mnist.py with Python
+```
+python mnist.py
+```
+
+## Author
+
+KARUNAKARAN Nithushan
+
+
+## License
+
+This project is licensed under the MIT License - see the LICENSE.md file for details
+
